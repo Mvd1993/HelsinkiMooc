eee82a9347a17a814e4fd1f8bafbcc7afd612088
Mvd1993 <52701546+Mvd1993@users.noreply.github.com>
2019-10-23 18:52:44 +0200

    Mooc Week3

---
+++ week3-044.Least/src/Least.java
@@ -0,0 +1,19 @@
+
+public class Least {
+
+    public static int least(int number1, int number2) {
+        // write program code here
+        // do not print anything inside the method
+        // method needs a return in the end
+        int least = number1;
+        if(number2 < number1){
+            least = number2;
+        }
+        return least;
+    }
+
+    public static void main(String[] args) {
+        int result = least(2, 7);
+        System.out.println("Least: " + result);
+    }
+}
